75f46aef47b332829eed01c3df43d925cb8a3c88
Sahil Painuly <166888935+sahil-painuly@users.noreply.github.com>
2024-12-01 20:39:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,9 @@ app.post('/api/remove-student', async (req, res) => {
 
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
